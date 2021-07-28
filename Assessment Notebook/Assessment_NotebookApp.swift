ba52d5a596c282b09a84253c5ca569d929b93c04
Cynthia Wang <CynthiaWang7979@hotmail.com>
2021-07-29 00:58:45 +0800

    Initial Commit

---
+++ Assessment Notebook/Assessment_NotebookApp.swift
@@ -0,0 +1,17 @@
+//
+//  Assessment_NotebookApp.swift
+//  Assessment Notebook
+//
+//  Created by Xia He on 2021/7/29.
+//
+
+import SwiftUI
+
+@main
+struct Assessment_NotebookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
